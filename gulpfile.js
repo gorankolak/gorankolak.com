00eeb0513b80b468309563dfa3492d435e1a47ed
gorankolak <goran.kolak82@gmail.com>
2015-03-02 10:03:06 +0100

    Add files

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload,
+    changed = require('gulp-changed'),
+    imagemin = require('gulp-imagemin');
+
+gulp.task('styles', function() {
+    var cssSrc = './scss/**/*.scss',
+        cssDest = './css';
+
+    return gulp.src(cssSrc)
+        .pipe(changed(cssDest))
+        .pipe(sass({
+            outputStyle: 'compressed',
+            sourceComments: 'map'
+        }))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(cssDest))
+        .pipe(reload({
+            stream:true
+        }));
+});
+
+gulp.task('imagemin', function() {
+    var imgSrc = './dev/images/**/*',
+        imgDest = './img';
+ 
+    return gulp.src(imgSrc)
+        .pipe(changed(imgDest))
+        .pipe(imagemin())
+        .pipe(gulp.dest(imgDest));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./scss/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles', 'imagemin', 'browser-sync', 'watch']);
