76cf3d6ca696cb43c069f6f39020082c8eca456a
gorankolak <goran.kolak82@gmail.com>
2015-03-02 10:14:27 +0100

    Add file

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+$(function() {
+    
+    // Show menu
+    $("#toggle").click(function() {
+        $(this).toggleClass("on");
+        $("#menu").slideToggle();
+    });
+
+    // Hide arrow
+    var windowHeight = window.innerHeight;
+    var iconArrow = $('.icon-arrow-down');
+
+    $(window).scroll(function() {
+        var iconDistance = iconArrow.offset().top;
+        if (iconDistance > windowHeight) {
+            iconArrow.stop(true).fadeOut();
+        } else {
+            iconArrow.stop(true).fadeIn();
+        }
+    });
+
+    // Scrolling effect
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    
+});
